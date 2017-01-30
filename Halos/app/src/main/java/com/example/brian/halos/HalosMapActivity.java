4cf374f4eb973a47bc0391e7e9e27907208530d2
Brian Heckman <brian@Brians-MacBook-Air.local>
2017-01-30 12:22:59 -0500

    add server outline, and fix play services version

--- Halos/app/src/main/java/com/example/brian/halos/HalosMapActivity.java
+++ Halos/app/src/main/java/com/example/brian/halos/HalosMapActivity.java
@@ -29,7 +29,7 @@ public class HalosMapActivity extends FragmentActivity implements OnMapReadyCall
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * we just add a marker near Syracuse, NY.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -39,8 +39,8 @@ public class HalosMapActivity extends FragmentActivity implements OnMapReadyCall
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng syracuse = new LatLng(43.0481, -76.1474);
+        mMap.addMarker(new MarkerOptions().position(syracuse).title("Marker in Syracuse, NY"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(syracuse));
     }
 }
