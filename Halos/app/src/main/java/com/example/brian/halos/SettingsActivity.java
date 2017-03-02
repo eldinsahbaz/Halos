d10cf4e35dd424d0ab2d8afeb315908e683f26d6
Brian Heckman <bheckman013@gmail.com>
2017-03-02 15:03:13 -0500

    improve settings ui

--- Halos/app/src/main/java/com/example/brian/halos/SettingsActivity.java
+++ Halos/app/src/main/java/com/example/brian/halos/SettingsActivity.java
@@ -4,10 +4,15 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONObject;
@@ -22,17 +27,31 @@ import okhttp3.Response;
 
 public class SettingsActivity extends AppCompatActivity {
     Button submitButton;
-    EditText radius;
-    String retVal;
-    OkHttpClient client = new OkHttpClient();
 
+    TextView radiusText;
+
+    SeekBar radiusBar;
+
+    int radius;
+
+    String retVal;
+
+    OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        radius = (EditText) findViewById(R.id.radius_input);
+        //Setup Toolbar
+        Toolbar toolbar = (Toolbar)findViewById(R.id.menu);
+        setSupportActionBar(toolbar);
+
+        radiusBar = (SeekBar) findViewById(R.id.settings_radius_bar);
+        radiusBar.setProgress(50);
+        radiusText = (TextView) findViewById(R.id.setting_radius_text);
+        radiusText.setText(radiusBar.getProgress()/10 + " Miles");
+        radius = radiusBar.getProgress();
 
         submitButton = (Button) findViewById(R.id.settings_submit_btn);
         submitButton.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +62,31 @@ public class SettingsActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        radiusBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 50;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+                radiusText.setText(progress/10 + " Miles");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.v("SeekBar", "Selected: " + progress);
+            }
+        });
+    }
+
+    public boolean onCreateOptionsMenu ( Menu menu ) {
+        getMenuInflater().inflate(R.menu.toolbar,menu );
+        return true ;
     }
 
     private class Setting extends AsyncTask<Void, Void, String> {
@@ -122,5 +166,39 @@ public class SettingsActivity extends AppCompatActivity {
 
         @Override
         protected void onProgressUpdate(Void... values) {}
+    }
+
+    @Override
+    public boolean onOptionsItemSelected ( MenuItem item ) {
+        switch( item.getItemId() ) {
+            case R.id.Home:
+                Intent intent1 = new Intent(this, HalosMapActivity.class);
+                startActivity(intent1);
+                return true ;
+            case R.id.store:
+                Intent intent2 = new Intent(this, StoreActivity.class);
+                startActivity(intent2);
+                return true ;
+            case R.id.profile:
+                Intent intent3 = new Intent(this, UserProfileActivity.class);
+                startActivity(intent3);
+                return true ;
+            case R.id.activity_settings:
+                Intent intent4 = new Intent(this, SettingsActivity.class);
+                startActivity(intent4);
+                return true ;
+            case R.id.Logout:
+                Intent intent6 = new Intent(this, LoginActivity.class);
+                startActivity(intent6);
+                return true ;
+            case R.id.create_tour:
+                Intent intent7 = new Intent(this, CreateTourActivity.class);
+                startActivity(intent7);
+                return true;
+            default :
+                // If we got here , the user ’s action was not recognized .
+                // Invoke the superclass to handle it .
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
